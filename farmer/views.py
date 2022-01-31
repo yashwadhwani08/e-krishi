43e666712426c5931bfd5a3671e430ae1dd6f94f
jatin0801 <58928905+jatin0801@users.noreply.github.com>
2022-01-31 23:30:59 +0530

    recommendation model

--- farmer/views.py
+++ farmer/views.py
@@ -11,6 +11,10 @@ from shop.models import Product,Rating
 from django.views.generic import CreateView
 from accounts.models import User,Farmer
 from django.contrib.auth.decorators import login_required
+import pickle
+from django.templatetags.static import static
+import joblib
+import os
 
 
 from django.contrib import messages
@@ -162,4 +166,55 @@ def StatewiseProductPrice(request):
 
 def cropDiseasePrediction(request):
     return render(request, "farmer/cropDiseasePrediction.html")
+
+def weather_fetch(city_name):
+    api_key = "6ee1dc7bb1bc5a8a16ba813050149c05"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+full_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'farmer/model/model.pkl')
+crop_recommendation_model = joblib.load(full_path)
+
+# def crop_recommendation_model(arr):
+#     #import pickle
+#     model = joblib.load('extra.sav')
+#     #scaled = joblib.load('encoder.sav')
+#     prediction = model.predict(arr)
+#     return prediction
+
+def crop_prediction(request):
+    title = 'e-Krishi: A one stop portal'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['potassium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+            print()
+            print("Final Prediction", final_prediction)
+            return render_template('crop-result.html', prediction=final_prediction.capitalize(), title=title)
+        else:
+            return render_template('try_again.html', title=title)
+
+
 
